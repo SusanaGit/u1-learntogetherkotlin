5b34a628e81c0c94687fecfb89930e52d66f99c6
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-09-30 01:00:11 +0200

    add text

--- app/src/main/java/com/example/learntogether/MainActivity.kt
+++ app/src/main/java/com/example/learntogether/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -15,10 +14,12 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.learntogether.ui.theme.LearnTogetherTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,9 +32,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingImage(
-                        modifier = Modifier.padding(8.dp)
-                    )
+                    ArticleCard()
                 }
             }
         }
@@ -41,23 +40,42 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingImage(modifier: Modifier = Modifier) {
-    val image = painterResource(R.drawable.bg_compose_background)
-
+fun ArticleCard(modifier: Modifier = Modifier) {
     Column (modifier) {
-        Image(
-            painter = image,
-            contentDescription = null,
-            modifier = Modifier.fillMaxWidth()
-        )
+        GreetingImage()
+        GreetingText()
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun GreetingImage() {
+    val image = painterResource(R.drawable.bg_compose_background)
+    Image(
+        painter = image,
+        contentDescription = null,
+        modifier = Modifier.fillMaxWidth()
+    )
+}
+
+@Composable
+fun GreetingText() {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = stringResource(R.string.first_element),
+        fontSize = 24.sp,
+        modifier = Modifier
+            .padding(16.dp)
+    )
+    Text(
+        text = stringResource(R.string.second_element),
+        textAlign = TextAlign.Justify,
+        modifier = Modifier
+            .padding(16.dp)
+    )
+    Text(
+        text = stringResource(R.string.third_element),
+        textAlign = TextAlign.Justify,
+        modifier = Modifier
+            .padding(16.dp)
     )
 }
 
@@ -65,6 +83,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun LearnTogetherPreview() {
     LearnTogetherTheme {
-        GreetingImage()
+        ArticleCard()
     }
 }
